4a11bf0357d9ff0db672c8ce61e8ac01c6d3723a
ethangabrielcalzita <ethangabrielcalzita16@gmail.com>
2026-06-03 10:37:58 +0800

    added whitenoise to serve static files

--- AegisDesk/settings.py
+++ AegisDesk/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -202,7 +203,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# The directory where collectstatic will gather all assets for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Enable WhiteNoise's compression and caching behavior for maximum speed
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
